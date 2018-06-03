30745dd386a66291c31530fd348e6be5f7e825a0
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-06-03 22:26:00 +0100

    mapping areas architecture from classical mvc to web api

---
+++ backend/HighElo/HighElo.WebAPI/Controllers/Booster/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HighElo.WebAPI.Controllers.Booster
+{
+    [Route("api/booster/[controller]")]
+    public class DashboardController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "booster1", "booster2" };
+        }
+    }
+}
