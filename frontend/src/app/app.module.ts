300e19634c0992fc20a7eb0cd7dd2c4cb5b35bb0
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-06-04 19:13:48 +0100

    added main route layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent} from './components/mainRoute/shared/menu/menu.component';
 import { HomeComponent } from './components/mainRoute/home/home.component';
+import { LayoutComponent as MainRouteLayout } from './components/mainRoute/shared/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
+    MainRouteLayout,
   ],
   imports: [
     BrowserModule
