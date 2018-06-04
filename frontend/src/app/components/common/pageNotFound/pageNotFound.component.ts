e92652f64131f4466de6e9d503bb63dc6060ffcb
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-06-04 20:23:40 +0100

    moved router-outlet to main route layout component

--- frontend/src/app/components/common/pageNotFound/pageNotFound.component.ts
+++ frontend/src/app/components/common/pageNotFound/pageNotFound.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    template: '<h1>The page you are looking for does not exist<h1>',
+    template: '<h1 style=\'margin-top:70px\'>The page you are looking for does not exist<h1>',
 })
 export class PageNotFoundComponent implements OnInit {
     constructor() { }
