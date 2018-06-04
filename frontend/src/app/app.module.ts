e937bbde30d1b92c523126fe65e1df804957fd25
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-06-04 18:49:41 +0100

    responsive menu added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent} from './components/mainRoute/shared/menu/menu.component';
 import { HomeComponent } from './components/mainRoute/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    MenuComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule
