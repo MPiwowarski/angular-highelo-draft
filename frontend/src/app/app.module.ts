f01ccb4479a055f541edc2e79378c66f162250e0
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-06-06 21:38:59 +0100

    app.modules.ts removed rendundant imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
+import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -25,14 +25,12 @@ const appRoutes: Routes = [
     HomeComponent,
     FaqComponent, 
     WinsBoostPurchaseComponent,
-    
+    MainRouteLayout,
   ],
   imports: [
-    MainRouteLayout,
     BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
