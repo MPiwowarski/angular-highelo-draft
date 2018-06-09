609289008965d6ffcbe69779d04b0f6dafbd2b79
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-06-10 00:42:26 +0100

    lint fixes

--- frontend/src/app/components/mainRoute/boostPurchase/duoQueuePurchase/duo-queue-purchase.component.ts
+++ frontend/src/app/components/mainRoute/boostPurchase/duoQueuePurchase/duo-queue-purchase.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-let OrderImages = require('../../../../partialViews/order-image.html');
+import { Component, AfterViewInit } from '@angular/core';
+const OrderImages = require('../../../../partialViews/order-image.html');
 
 @Component({
   selector: 'app-duo-queue-purchase',
   templateUrl: './duo-queue-purchase.component.html',
   styleUrls: ['./duo-queue-purchase.component.scss']
 })
-export class DuoQueuePurchaseComponent  {
+export class DuoQueuePurchaseComponent implements AfterViewInit {
   public sliderVal = 1;
   public orderImages: string;
   public orderImage: string;
@@ -16,9 +16,9 @@ export class DuoQueuePurchaseComponent  {
   }
 
   ngAfterViewInit() {
-    var unrankedImageSrc = (<HTMLImageElement>document.getElementsByClassName('Unranked_I')[0]).src;
+    const unrankedImageSrc = (<HTMLImageElement>document.getElementsByClassName('Unranked_I')[0]).src;
     console.log(unrankedImageSrc);
-    this.orderImage = "<image src=\"" + unrankedImageSrc + "\" />";
+    this.orderImage = '<image src=\'' + unrankedImageSrc + '\' />';
     console.log(this.orderImage);
   }
 
