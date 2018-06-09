52580b29c5590be36f00d4732cabfb5086b5f7a0
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-06-09 14:35:14 +0100

    angular range slider lib added to proj

--- frontend/src/app/components/mainRoute/boostPurchase/duoQueuePurchase/duo-queue-purchase.component.ts
+++ frontend/src/app/components/mainRoute/boostPurchase/duoQueuePurchase/duo-queue-purchase.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-duo-queue-purchase',
   templateUrl: './duo-queue-purchase.component.html',
   styleUrls: ['./duo-queue-purchase.component.scss']
 })
-export class DuoQueuePurchaseComponent implements OnInit {
+export class DuoQueuePurchaseComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  onChangeAchievement(eventStr: string, eRef) {
 
+     alert('eventStr ' + eventStr)
+
+  }
 }
